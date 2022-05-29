d5d7a83c55264a10bc3039fde81070a1c96bf8b9
Juan David Salazar <tipito4085@gmail.com>
2022-05-29 13:57:20 -0500

    Scroll reveal

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.sr =ScrollReveal();
+
+    sr.reveal('#navbar', {
+        duration: 1200,
+        origin: 'bottom',
+        distance: '-200px'
+    });
+    sr.reveal('.scroll-banner', {
+        duration: 1300,
+        origin: 'top',
+        distance: '-200px'
+    });
+    sr.reveal('.scroll-banner1', {
+        duration: 1300,
+        origin: 'left',
+        distance: '-50px',
+        opacity: 0
+    });
+    sr.reveal('.scroll-footer', {
+        duration: 1800,
+        origin: 'bottom',
+        distance: '200px',
+        delay: 500,
+    });
